39bec88d02547ba99a32ef8b600c6327ffb22c96
Ged Dackys <ged@bloomandwild.com>
2021-08-19 16:09:58 +0100

    Add sad path specs for PrinterLabelRequest

--- spec/sendcloud/operations/printer_label_request_spec.rb
+++ spec/sendcloud/operations/printer_label_request_spec.rb
@@ -10,17 +10,33 @@ RSpec.describe Sendcloud::Operations::PrinterLabelRequest do
   end
 
   describe "#execute" do
-    # taken from create_parcel_request/valid_request
-    let(:parcel_id) { "124292020" }
     let(:subject) { described_class.new(parcel_id: parcel_id) }
 
-    it "returns a response in the expected format" do
-      VCR.use_cassette("printer_label_request/valid_request") do
-        result = subject.execute
+    context "valid request" do
+      # taken from create_parcel_request/valid_request
+      let(:parcel_id) { "124292020" }
 
-        aggregate_failures do
-          expect(subject.response.status).to eq(200)
-          expect(Base64::strict_decode64(result)).to match("%PDF") # checking for PDF header
+      it "returns a response in the expected format" do
+        VCR.use_cassette("printer_label_request/valid_request") do
+          result = subject.execute
+
+          aggregate_failures do
+            expect(subject.response.status).to eq(200)
+            expect(Base64::strict_decode64(result)).to match("%PDF") # checking for PDF header
+          end
+        end
+      end
+    end
+
+    context "with incorrect parcel ID" do
+      let(:parcel_id) { "1" }
+
+      it "raises an exception" do
+        VCR.use_cassette("printer_label_request/invalid_request") do
+          aggregate_failures do
+            expect { subject.execute }.to raise_error(Sendcloud::ResponseError, "404 No Parcel matches the given query.")
+            expect(subject.response.status).to eq(404)
+          end
         end
       end
     end
