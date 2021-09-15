e3b27f7ade5e1419a8d9ceecb6b807970c2085a6
Ged Dackys <ged@bloomandwild.com>
2021-09-15 15:16:45 +0100

    Add support for per-request credentials to CreateShipmentRequest

--- spec/sendcloud/operations/create_shipment_request_spec.rb
+++ spec/sendcloud/operations/create_shipment_request_spec.rb
@@ -35,18 +35,38 @@ RSpec.describe Sendcloud::Operations::CreateShipmentRequest do
 
     context "valid request" do
       it "returns a response in the expected format" do
-        VCR.use_cassette("create_parcel_request/valid_request") do
-          VCR.use_cassette("printer_label_request/valid_request") do
-            result = subject.execute
+        VCR.use_cassette("create_shipment/valid_request") do
+          result = subject.execute
 
-            aggregate_failures do
-              expect(subject.response.status).to eq(200)
+          aggregate_failures do
+            expect(subject.response.status).to eq(200)
 
-              expect(result).to be_instance_of(Hash)
-              expect(result.keys).to include(:id, :label, :tracking_number, :tracking_url, :pdf_label)
-              expect(Base64.strict_decode64(result[:pdf_label])).to match("%PDF") # checking for PDF header
-            end
+            expect(result).to be_instance_of(Hash)
+            expect(result.keys).to include(:id, :label, :tracking_number, :tracking_url, :pdf_label)
+            expect(Base64.strict_decode64(result[:pdf_label])).to match("%PDF") # checking for PDF header
           end
+        end
+      end
+    end
+
+    context "with local credentials" do
+      before do
+        # public_key and secret_key passed as arguments take precedence over the global config
+        configure_client(base_url: default_base_url, public_key: "foo", secret_key: "bar")
+      end
+
+      let(:subject) do
+        described_class.new(
+          payload: payload,
+          public_key: ENV.fetch("SENDCLOUD_PUBLIC_KEY", ""),
+          secret_key: ENV.fetch("SENDCLOUD_SECRET_KEY", "")
+        )
+      end
+
+      it "still works" do
+        VCR.use_cassette("create_shipment/valid_request_with_local_credentials") do
+          subject.execute
+          expect(subject.response.status).to eq(200)
         end
       end
     end
